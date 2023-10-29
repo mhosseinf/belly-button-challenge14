8eec4e7eb6656a8990ff946d844fda2d2aa4fd8a
mhosseinf <mh30f@yahoo.com>
2023-10-29 15:34:49 +0800

    Function to create the bar chart completed

--- Interactive-Visualisations/static/js/app.js
+++ Interactive-Visualisations/static/js/app.js
@@ -1,4 +1,40 @@
-// Separate function to handle dropdown change
+// Function to create the bar chart
+function createBarChart(data, selectedOTU) {
+    // Find the selected OTU data from the samples array
+    let otus = data.samples.filter(sample => sample.id === selectedOTU);
+
+    // Sort the otus in descending order based on otu_ids
+    otus.sort((a, b) => b.otu_ids - a.otu_ids);
+
+    // Get the top 10 OTU IDs, sample values, and labels
+    let topOTUs = otus.slice(0, 10);
+    
+    // Extract the necessary data for the bar chart
+    let otuIds = topOTUs.map(otu => `OTU ${otu.otu_ids}`);
+    let sampleValues = topOTUs.map(otu => otu.sample_values);
+    let otuLabels = topOTUs.map(otu => otu.otu_labels);
+
+    // Define the trace for the bar chart
+    let trace = {
+        x: sampleValues,
+        y: otuIds,
+        text: otuLabels,
+        type: 'bar',
+        orientation: 'h',
+    };
+
+    // Define layout for the bar chart
+    let layout = {
+        title: `Top 10 OTUs for ${selectedOTU}`,
+    };
+
+    // Create an array with the trace
+    let dataToPlot = [trace];
+    // Create the bar chart using Plotly
+    Plotly.newPlot("bar", dataToPlot, layout);
+}
+
+// Function to handle dropdown change
 function dropdownChange(data, selectedOTU) {
     createBarChart(data, selectedOTU);
     updateBubbleChart(data, selectedOTU);
@@ -13,7 +49,7 @@ let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1
 function init() {
     // Fetch the JSON data from the above URL
     d3.json(url).then(data => {
-        console.log("Fetched data:", data); // Log fetched data
+        console.log("Fetched data:", data);
         // Extract the list of OTU IDs
         let otuIds = data.names;
 
@@ -28,12 +64,17 @@ function init() {
             option.text(`OTU ${otuId}`);
         }
 
-        // Define event for dropdown change
-        dropdownMenu.on("change", function() {
-            let selectedOTU = dropdownMenu.property("value");
-            dropdownChange(data, selectedOTU); // Call the function to update charts
+        // Define event for dropdown change and Call the function to update charts
+        otuDropdown.on("change", function() {
+            let selectedOTU = otuDropdown.property("value");
+            dropdownChange(data, selectedOTU); 
         });
 
+        // Create the initial bar chart, bubble chart, demographic info, and gauge chart with the first OTU
+        createBarChart(data, otuIds[0]);
+        updateBubbleChart(data, otuIds[0]);
+        displayDemographicInfo(data, otuIds[0]);
+        updateGaugeChart(data, otuIds[0]);
     })
 }
 
