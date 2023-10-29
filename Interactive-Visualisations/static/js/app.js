ddd915c702e88e98b800c39c5d7669a7d1f58a96
mhosseinf <mh30f@yahoo.com>
2023-10-29 18:09:38 +0800

    Function to create the bubble chart created

--- Interactive-Visualisations/static/js/app.js
+++ Interactive-Visualisations/static/js/app.js
@@ -1,11 +1,9 @@
 // Function to create the bar chart
 function createBarChart(data, selectedOTU) {
     console.log("Selected OTU data:", data); // Log selected OTU data
-    
     // Find the selected OTU data from the samples array using filter
     let otus = data.samples.filter(sample => sample.id === selectedOTU)[0];
     console.log("Selected OTU:", otus); // Log selected OTU
-    
     // Get the top 10 OTU IDs in reverse order
     let topOTUs = otus.otu_ids.slice(0, 10).reverse();
     // Get the top 10 OTU labels in reverse order
@@ -30,10 +28,49 @@ function createBarChart(data, selectedOTU) {
     // Create the bar chart using Plotly
     Plotly.newPlot("bar", dataToPlot, layout);
 }
+
+// Function to create the bubble chart
+function createBubbleChart(data, selectedOTU) {
+    console.log("Selected OTU data:", data); // Log selected OTU data
+    
+    // Find the selected OTU data from the samples array using filter
+    let otus = data.samples.filter(sample => sample.id === selectedOTU)[0];
+    console.log("Selected OTU:", otus); // Log selected OTU
+    
+    // Define the trace for the bubble chart
+    let trace = {
+        x: otus.otu_ids,
+        y: otus.sample_values,
+        text: otus.otu_labels,
+        mode: 'markers',
+        marker: {
+            size: otus.sample_values,
+            color: otus.otu_ids,
+            colorscale: 'Earth'
+        }
+    };
+
+    // Define layout for the bubble chart
+    let layout = {
+        title: `Bubble Chart for ${selectedOTU}`,
+        xaxis: {
+            title: 'OTU ID'
+        },
+        yaxis: {
+            title: 'Sample Values'
+        }
+    };
+
+    // Create an array with the trace
+    let dataToPlot = [trace];
+    // Create the bubble chart using Plotly
+    Plotly.newPlot("bubble", dataToPlot, layout);
+}
+
 // Function to handle dropdown change
 function dropdownChange(data, selectedOTU) {
     createBarChart(data, selectedOTU);
-    // updateBubbleChart(data, selectedOTU);
+    createBubbleChart(data, selectedOTU);
     // displayDemographicInfo(data, selectedOTU);
     // updateGaugeChart(data, selectedOTU);
 }
@@ -60,18 +97,16 @@ function init() {
             option.text(`OTU ${otuId}`);
         }
 
-        // Define event for dropdown change and Call the function to update charts
+        // Define event for dropdown change and call the function to update charts
         otuDropdown.on("change", function() {
             let selectedOTU = otuDropdown.property("value");
             dropdownChange(data, selectedOTU); 
         });
 
-        // Create the initial bar chart, bubble chart, demographic info, and gauge chart with the first OTU
+        // Create the initial bar chart and bubble chart with the first OTU
         createBarChart(data, otuIds[0]);
-        // updateBubbleChart(data, otuIds[0]);
-        // displayDemographicInfo(data, otuIds[0]);
-        // updateGaugeChart(data, otuIds[0]);
-    })
+        createBubbleChart(data, otuIds[0]);
+    });
 }
 
 // Call the initialization function
